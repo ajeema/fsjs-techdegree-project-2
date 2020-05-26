88432ec2142d1c91fb1fd08fc2cc76bbb2f8a08d
Aaron Jorgensen <mraaronjorgensen@gmail.com>
2020-05-26 16:00:26 -0700

    add comments

--- js/script.js
+++ js/script.js
@@ -1,45 +1,15 @@
 /******************************************
+ * Aaron Jorgensen
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-var page = document.querySelector(".page");
-var pageHeader = document.querySelector(".page-header");
-var student = document.querySelectorAll(".student-item");
+//global variables
 var studentsPerPage = 10;
 var studentListUl = document.querySelectorAll(".student-list")[0];
 var studentListLi = studentListUl.children;
 
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-const showPage = (list, page) => {
+// define showing or hiding students
+function showPage (list, page) {
     const min = page * studentsPerPage;
     const max = (page + 1) * studentsPerPage;
     for (let i = 0; i < list.length; ++i) {
@@ -52,14 +22,11 @@ const showPage = (list, page) => {
 }
 
 
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-const appendPageLinks = (list) => {
+// dynamically create pages and append list to them
+function appendPageLinks (list) {
+    // find the total number of students
     const numberOfStudents = studentListLi.length;
+    // divide number of found students by number of defined students per page set in global var
     const amountOfPages = Math.ceil(numberOfStudents / studentsPerPage);
     const newDiv = document.createElement("div");
     newDiv.className = "pagination";
@@ -85,12 +52,8 @@ const appendPageLinks = (list) => {
     }
     anchorList[0].className = "active";
 
-
 }
+
+// call the functions to initiate them
 appendPageLinks();
 showPage(studentListLi, 0);
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
