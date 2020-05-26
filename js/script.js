9fbfac321e463d356e8ecd16e0b0f52e3ff0051c
Aaron Jorgensen <mraaronjorgensen@gmail.com>
2020-05-26 14:11:13 -0700

    add pagination

--- js/script.js
+++ js/script.js
@@ -16,7 +16,12 @@ FSJS project 2 - List Filter and Pagination
    will only be used inside of a function, then it can be locally 
    scoped to that function.
 ***/
-
+var page = document.querySelector(".page");
+var pageHeader = document.querySelector(".page-header");
+var student = document.querySelectorAll(".student-item");
+var studentsPerPage = 10;
+var studentListUl = document.querySelectorAll(".student-list")[0];
+var studentListLi = studentListUl.children;
 
 
 
@@ -34,6 +39,17 @@ FSJS project 2 - List Filter and Pagination
        that will be passed into the parens later when you call or 
        "invoke" the function 
 ***/
+const showPage = (list, page) => {
+    const min = page * studentsPerPage;
+    const max = (page + 1) * studentsPerPage;
+    for (let i = 0; i < list.length; ++i) {
+        if (i >= min && i < max) {
+            list[i].style.display = "block";
+        } else {
+            list[i].style.display = "none";
+        }
+    }
+}
 
 
 
@@ -42,7 +58,37 @@ FSJS project 2 - List Filter and Pagination
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
+const appendPageLinks = (list) => {
+    const numberOfStudents = studentListLi.length;
+    const amountOfPages = Math.ceil(numberOfStudents / studentsPerPage);
+    const newDiv = document.createElement("div");
+    newDiv.className = "pagination";
+    const mainPage = document.querySelectorAll(".page")[0];
+    mainPage.appendChild(newDiv);
+    const newDivUl = document.createElement("ul");
+    newDiv.appendChild(newDivUl);
+    const anchorList = document.getElementsByTagName("a");
+    for (let i = 0; i < amountOfPages; ++i) {
+        const newDivLi = document.createElement("li");
+        const anchor = document.createElement("a");
+        newDivUl.appendChild(newDivLi);
+        anchor.href = "#";
+        anchor.textContent = i + 1;
+        newDivLi.appendChild(anchor);
+        anchor.addEventListener("click", (e) => {
+            for (let i = 0; i < anchorList.length; i++) {
+                anchorList[i].className = "";
+                e.target.className = "active";
+            }
+            showPage(studentListLi, i);
+        });
+    }
+    anchorList[0].className = "active";
 
+
+}
+appendPageLinks();
+showPage(studentListLi, 0);
 
 
 
